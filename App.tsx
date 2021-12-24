cbcee4f82132dc0d2272b25cf46ceeda0abd80b4
Laecio12 <laecio12silva@gmail.com>
2021-12-24 08:59:35 -0300

    bug da hora

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Dashboard} from './src/screens/Dashboard';
 import {ThemeProvider} from 'styled-components';
 import theme from './src/global/styles/theme';
-
+import SplashScreen from 'react-native-splash-screen';
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  } , []);
+  
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
--- src/components/CreateAlarms/index.js
+++ src/components/CreateAlarms/index.js
@@ -48,16 +48,28 @@ export  function CreateAlarms() {
 
   function minutesMinusTen(isoDate) {
     let minutes = isoDate.getMinutes();
-    let hours = isoDate.getHours()
+    let hours = isoDate.getHours();
+    let day = isoDate.getDate();
 
     // para compensar o fuso horário
     if (hours <= 20) hours += 3;
-    else if (hours ===  21) hours += 2;
-    else if (hours ===  22) hours += 1;
-    
-    if(hours > 23) return;
+    else if (hours ===  21){
+      day += 1;
+      hours = 0;
+    }
+    else if (hours ===  22) {
+      day += 1;
+      hours = 1;
+    }else if (hours ===  23) {
+      day += 1;
+      hours = 2;
+    }
 
-    if (minutes <= 9) {
+    if(hours === 0 && minutes <= 9) {
+      minutes = `5${minutes}`;
+      hours = 23;
+      day -= 1;
+    } else if (minutes <= 9) {
       minutes = `5${minutes}`;
       hours -= 1;
     } else{
@@ -67,8 +79,12 @@ export  function CreateAlarms() {
     if(minutes <= 9){
      minutes = `0${minutes}`;
     }
-
-    return new Date(`${isoDate.getFullYear()}-${isoDate.getMonth() + 1}-${isoDate.getDate()}T${hours}:${minutes}:00.000Z`);
+    if(hours <= 9){
+     hours = `0${hours}`;
+    }
+    console.log(`${hours}:${minutes}`);
+    console.log(`${isoDate.getFullYear()}-${isoDate.getMonth() + 1}-${day}T${hours}:${minutes}:00.000Z`)
+    return new Date(`${isoDate.getFullYear()}-${isoDate.getMonth() + 1}-${day}T${hours}:${minutes}:00.000Z`);
     
   }
 
@@ -225,6 +241,7 @@ export  function CreateAlarms() {
   };
    
   async function closingDayAlert(datePiked)  {
+    console.log(datePiked)
     const hourLimit = new Date(`${dateToday}T23:00:00.000Z`)
     const closingDayData = {
       fireDate:  datePiked || new Date(`${dateToday}T22:20:00.000Z`),//19:20:00
@@ -233,6 +250,18 @@ export  function CreateAlarms() {
     if(datePiked){
       const hourPiked = datePiked.getHours();
       const minutesPiked = zeroInMinutes(datePiked);
+      if(hourPiked < currentDate) {
+        Alert.alert(
+          'Olá Tryber!',
+          `Não é possível adicionar no passado!😆`,
+          [
+            {text: 'Escolher novamente',  onPress: () =>  showDateTimePicker()},
+            {text: 'Não adicionar', onPress: () => {return}},
+          ],
+          
+        );
+        return;
+      }
       if(`${hourPiked}:${minutesPiked}` >= '18:50' && hourPiked < 20){
         handleCreateAlarm(closingDayData.fireDate, closingDayData.message);
         Alert.alert("Tudo certo tryber!", "Agenda ok, #VQV!");
