6137e5cb36fd9ec25c42c64ed80998bea973bdd3
Laecio12 <laecio12silva@gmail.com>
2022-01-04 11:41:10 -0300

    corrigindo bug exibição do alert tema escuro

--- src/components/ListAlarms/index.tsx
+++ src/components/ListAlarms/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactNativeAN from '../../react-native-alarm-notification';
+import { zeroInLessTen } from "../../services/zeroInLessTen";
 
 import { AlarmItem } from "../AlarmItem";
 import {Container, AlarmList} from "./styles";
@@ -25,9 +26,9 @@ export function ListAlarms() {
     
     //Remove automaticamente o alarme uma hora depois de disparar.
     alarmsList.forEach((alarm: IDataProps )=> {
-      const dateAlarm = new Date(`${alarm.year}-${alarm.month}-${alarm.day}T${alarm.hour + 4}:${alarm.minute}:00.000Z`);
+      const dateAlarm = new Date(`${alarm.year}-${zeroInLessTen(alarm.month)}-${zeroInLessTen(alarm.day)}T${zeroInLessTen(alarm.hour + 4)}:${zeroInLessTen(alarm.minute)}:00.000Z`);
       const dateNow = new Date();
-
+      
       if(dateAlarm < dateNow){
         ReactNativeAN.deleteAlarm(Number(alarm.id));
       }
